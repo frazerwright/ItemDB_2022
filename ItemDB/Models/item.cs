708ceaef4dc01b7edb49aee39155601f062aca62
Frazer Wright <ac111503@avcol.school.nz>
2022-03-07 15:05:56 +1300

    first model(?)

---
+++ ItemDB/Models/item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ItemDB.Models
+{
+    public class item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Rarity { get; set; }
+        public string Description { get; set; }
+        public string Personal_Notes { get; set; }
+        public string Archetype { get; set; }
+ 
+
+       
+    }
+}
