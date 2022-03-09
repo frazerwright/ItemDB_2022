24ca5c0cd3dc641880cac542265e5aa884d11966
Frazer Wright <ac111503@avcol.school.nz>
2022-03-10 11:10:44 +1300

    added context(?)

---
+++ ItemDB/Data/itemContext.cs
@@ -0,0 +1,17 @@
+﻿using ItemDB.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ItemDB.Data
+{
+    public class itemContext : DbContext
+    {
+        public itemContext(DbContextOptions<itemContext> options) : base(options)
+        {
+
+        }
+        public DbSet<item> Items { get; set; }
+        public DbSet<order> Orders { get; set; }
+        public DbSet<recipient> recipients { get; set; }
+
+    }
+}
